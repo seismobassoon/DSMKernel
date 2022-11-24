0a7cdf7b31323158d472d09e9f4b28de3203f5c2
Nobuaki Fuji <seismobassoon@gmail.com>
2022-11-24 12:14:49 +0100

    cleaned up till the end, now working for iCompute =2 and to get waveform partials (and volume)

--- src/KernelMaker/localschema.f90
+++ src/KernelMaker/localschema.f90
@@ -1195,3 +1195,286 @@ subroutine localMetaComputeKernel
   
   return
 end subroutine localMetaComputeKernel
+
+
+subroutine compileKernelOutput
+  use localParamKernel
+  use parametersKernel
+  use tmpSGTs
+  use angles
+  use kernels
+  use rotate
+  implicit none
+
+  if(trim(paramWRT).eq.'vRSGT') then
+     allocate(totalker(nr,nphi,ntheta,1:num_h3,0:nfilter))
+  elseif(trim(paramWRT).eq.'vTSGT') then
+     allocate(totalker(nr,nphi,ntheta,1:num_h4,0:nfilter))
+  elseif((trim(paramWRT).eq.'alphaV').or.(trim(paramWRT).eq.'betaV').or.(trim(paramWRT).eq.'allV')) then
+     allocate(totalker(nr,nphi,ntheta,0:nkvtype,0:nfilter))
+  else
+     allocate(totalker(nr,nphi,ntheta,0:nktype,0:nfilter))
+  endif
+
+  if(my_rank.eq.0) then   
+     if((trim(paramWRT).ne.'vRSGT').and.(trim(paramWRT).ne.'vTSGT').and.(trim(paramWRT).ne.'alphaV').and.&
+          (trim(paramWRT).ne.'betaV').and.(trim(paramWRT).ne.'allV')) then
+        totalker = 0.e0
+        do ir=1,nr
+           write(tmpchar,'(I7)') int(r(ir)*1.d3)
+           do j=1,7
+              if(tmpchar(j:j).eq.' ') tmpchar(j:j) = '0'
+           enddo
+           
+           kerfile = trim(parentDir)//"/tmp/"//trim(stationName)//"."//trim(eventName)//"."//trim(phase)//"."//trim(compo)//"."//trim(tmpchar)
+           
+           open(1,file=kerfile,status='unknown',form='unformatted', &
+                access = 'direct', recl=kind(0e0)*nphi*ntheta*(nktype+1)*(nfilter+1))    
+           read(1,rec=1) totalker(ir,1:nphi,1:ntheta,0:nktype,0:nfilter)
+           close(1) 
+        enddo
+ 
+
+        do ift = 0,nfilter
+           kertotalfile = trim(parentDir)//trim(stationName)//"."//trim(eventName)//"."//trim(phase)//"."//trim(compo)//"."//trim(freqid(ift))//trim(".kernel")
+           open(1,file=kertotalfile,status='unknown',form='unformatted',access='sequential')
+           if(compo.eq.'Z') then
+              kc=1
+           elseif(compo.eq.'R') then
+              kc=2
+           elseif(compo.eq.'T') then
+              kc=3
+           else
+              kc=-1
+           endif
+           idum=0
+           fdum=0.e0
+           
+           write(1) totalker(:,:,:,:,ift)
+           close(1) 
+        enddo
+     endif
+
+  endif
+
+  
+
+  if(trim(paramWRT).eq.'vRSGT') then
+     
+     do jt=1,number_of_snapshots
+
+        if(((mod(jt-my_rank-ir,2*nproc).eq.0).or.(mod(jt+my_rank+1-ir,2*nproc).eq.0))) then
+           
+           totalker=0.e0
+           do ir=1,nr
+              do ith=1,ntheta
+                       
+
+                 write(tmpchar,'(I7,".",I7,".",I7)') ir,ith,jt
+                 do j=1,23
+                    if(tmpchar(j:j).eq.' ') tmpchar(j:j) = '0'
+                 enddo
+                 kerfile=trim(parentDir)//"/tmpvideoparts/"//trim(stationName)//"."//trim(eventName)//"."//trim(phase)//"."//trim(compo)//"."//trim(tmpchar)&
+                      //"."//"timemarching"
+                 open(1,file=kerfile,status='unknown',form='unformatted',access='direct',recl=kind(0e0)*nphi*(num_h3)*(1+nfilter))
+                 read(1,rec=1) videoker(1:nphi,1:num_h3,0:nfilter,1)
+                 close(1) 
+                 totalker(ir,1:nphi,ith,1:num_h3,0:nfilter)=videoker(1:nphi,1:num_h3,0:nfilter,1)
+              enddo
+           enddo
+
+                 
+           do ift = 0,nfilter
+              write(tmpchar,'(I7)') jt
+              do j=1,7
+                 if(tmpchar(j:j).eq.' ') tmpchar(j:j) = '0'
+              enddo
+              kertotalfile = trim(parentDir)//"/tmpvideo/"//trim(stationName)//"."//trim(eventName)//"."//trim(phase)//"."//trim(compo)//"." &
+                   //trim(freqid(ift))//"."//trim(tmpchar)//trim(".video")
+              open(1,file=kertotalfile,status='unknown',form='unformatted',access='sequential')
+              if(compo.eq.'Z') then
+                 kc=1
+              elseif(compo.eq.'R') then
+                 kc=2
+              elseif(compo.eq.'T') then
+                 kc=3
+              else
+                 kc=-1
+              endif
+              idum=0
+              fdum=0.e0
+              
+              write(1) totalker(1:nr,1:nphi,1:ntheta,1:num_h3,ift)
+              close(1) 
+              
+           enddo
+         
+        endif
+
+     enddo
+  endif
+
+
+  if(trim(paramWRT).eq.'vTSGT') then
+     
+     do jt=1,number_of_snapshots
+        if(((mod(jt-my_rank-ir,2*nproc).eq.0).or.(mod(jt+my_rank+1-ir,2*nproc).eq.0))) then
+           totalker=0.e0
+           do ir=1,nr
+              do ith=1,ntheta
+                 write(tmpchar,'(I7,".",I7,".",I7)') ir,ith,jt
+                 do j=1,23
+                    if(tmpchar(j:j).eq.' ') tmpchar(j:j) = '0'
+                 enddo
+                 
+                 kerfile=trim(parentDir)//"/tmpvideoparts/"//trim(stationName)//"."//trim(eventName)//"."//trim(phase)//"."//trim(compo)//"."//trim(tmpchar)&
+                      //"."//"timemarching"
+                 open(1,file=kerfile,status='unknown',form='unformatted',access='direct',recl=kind(0e0)*nphi*(num_h3)*(1+nfilter))
+                 read(1,rec=1) videoker(1:nphi,1:num_h4,0:nfilter,1)
+                 close(1) 
+                 totalker(ir,1:nphi,ith,1:num_h4,0:nfilter)=videoker(1:nphi,1:num_h4,0:nfilter,1)
+              enddo
+           enddo
+
+                 
+           do ift = 0,nfilter
+              write(tmpchar,'(I7)') jt
+              do j=1,7
+                 if(tmpchar(j:j).eq.' ') tmpchar(j:j) = '0'
+              enddo
+              kertotalfile = trim(parentDir)//"/tmpvideo/"//trim(stationName)//"."//trim(eventName)//"."//trim(phase)//"."//trim(compo)//"." &
+                   //trim(freqid(ift))//"."//trim(tmpchar)//trim(".video")
+              open(1,file=kertotalfile,status='unknown',form='unformatted',access='sequential')
+              if(compo.eq.'Z') then
+                 kc=1
+              elseif(compo.eq.'R') then
+                 kc=2
+              elseif(compo.eq.'T') then
+                 kc=3
+              else
+                 kc=-1
+              endif
+              idum=0
+              fdum=0.e0
+              
+              write(1) totalker(1:nr,1:nphi,1:ntheta,1:num_h4,ift)
+              close(1) 
+              
+           enddo
+           
+        endif
+     enddo
+  endif
+     
+  if((trim(paramWRT).eq.'alphaV').or.(trim(paramWRT).eq.'betaV').or.(trim(paramWRT).eq.'allV')) then
+           
+     do jt=1,number_of_snapshots
+        if(((mod(jt-my_rank-ir,2*nproc).eq.0).or.(mod(jt+my_rank+1-ir,2*nproc).eq.0))) then
+           totalker=0.e0
+           do ir=1,nr
+              do ith=1,ntheta
+                 
+                 write(tmpchar,'(I7,".",I7,".",I7)') ir,ith,jt
+                 do j=1,23
+                    if(tmpchar(j:j).eq.' ') tmpchar(j:j) = '0'
+                 enddo
+                 
+                 kerfile=trim(parentDir)//"/tmpvideoparts/"//trim(stationName)//"."//trim(eventName)//"."//trim(phase)//"."//trim(compo)//"."//trim(tmpchar)&
+                      //"."//"timemarching"
+                 open(1,file=kerfile,status='old',form='unformatted',access='direct',recl=kind(0e0)*nphi*(1+nkvtype)*(1+nfilter))
+                 read(1,rec=1) videoker(1:nphi,0:nkvtype,0:nfilter,1)
+                 close(1)
+                 totalker(ir,1:nphi,ith,0:nkvtype,0:nfilter)=videoker(1:nphi,0:nkvtype,0:nfilter,1)
+
+                 ! something that we can try :
+                 ! open(1,file=kerfile,status='old',form='unformatted',access='direct',recl=kind(0e0)*nphi*(1+nkvtype)*(1+nfilter))
+                 ! read(1,rec=jt) totalker(ir,1:nphi,ith,0:nkvtype,0:nfilter)
+                
+              enddo
+           enddo
+           
+           
+           do ift = 0,nfilter
+              write(tmpchar,'(I7)') jt
+              do j=1,7
+                 if(tmpchar(j:j).eq.' ') tmpchar(j:j) = '0'
+              enddo
+              kertotalfile = trim(parentDir)//"/tmpvideo/"//trim(stationName)//"."//trim(eventName)//"."//trim(phase)//"."//trim(compo)//"." &
+                   //trim(freqid(ift))//"."//trim(tmpchar)//trim(".video")
+              open(1,file=kertotalfile,status='unknown',form='unformatted',access='sequential')
+              if(compo.eq.'Z') then
+                 kc=1
+              elseif(compo.eq.'R') then
+                 kc=2
+              elseif(compo.eq.'T') then
+                 kc=3
+              else
+                 kc=-1
+              endif
+              idum=0
+              fdum=0.e0
+              
+              write(1) totalker(1:nr,1:nphi,1:ntheta,0:nkvtype,ift)
+              close(1) 
+              
+           enddo
+        endif
+
+     enddo
+                 
+  endif
+  
+  deallocate(totalker)
+  return
+end subroutine compileKernelOutput
+
+
+subroutine finalLogMessage
+  use localParamKernel
+  use parametersKernel
+  use tmpSGTs
+  use angles
+  use kernels
+  use rotate
+  implicit none
+
+  if(my_rank.eq.0) then
+     
+     if(trim(paramWRT).eq.'vRSGT') then
+        nktype_real=num_h3-1
+     elseif(trim(paramWRT).eq.'vTSGT') then
+        nktype_real=num_h4-1
+     elseif((trim(paramWRT).eq.'alphaV').or.(trim(paramWRT).eq.'betaV').or.(trim(paramWRT).eq.'allV')) then
+        nktype_real=nkvtype
+     else
+        nktype_real=nktype
+     endif
+     
+     
+     ift=nfilter
+
+     infofile = trim(parentDir)//trim(stationName)//"."//trim(eventName)//"."//&
+                trim(phase)//"."//trim(compo)//trim(".info")
+     open(1,file=infofile,status='unknown',form='unformatted',access='sequential')
+     write(1) nr,nphi,ntheta,nktype_real,nfilter,iWindowEnd-iWindowStart+1,number_of_snapshots
+     write(1) real(t(nt1(ift))),real(t(nt2(ift)))
+     write(1) (real(mt(i)),i=1,6)
+     write(1) real(r0D)
+     write(1) real(u)
+     close(1)
+     
+    
+
+     list = trim(parentDir)//"/log/calLog"//"."// &
+          trim(stationName)//"."//trim(eventName)//"."//trim(compo)//"."//trim(paramWRT)//".log"   
+     open(1,file =list, status = 'old',access='append', form = 'formatted')
+     call date_and_time(datex,timex)
+     write(1,'(/a,a4,a,a2,a,a2,a,a2,a,a2,a,a4)') &
+          '    Finishing date and time:                     ', &
+          datex(1:4),'-',datex(5:6),'-',datex(7:8),'.  ', &
+          timex(1:2),':',timex(3:4),':',timex(5:8)   
+     close (1)
+     
+  endif
+  return
+end subroutine finalLogMessage
