843ec8ba8745e13bfa165ffa4061f2a6801fc02e
Nobuaki Fuji <seismobassoon@gmail.com>
2023-03-09 17:57:22 +0100

    qkappa ??

--- src/SGTpsvMax/modules.f90
+++ src/SGTpsvMax/modules.f90
@@ -142,6 +142,7 @@ subroutine bcast_allocate_1
   
   if(my_rank.eq.0) then
      tmpfile='argvModeUsed'
+     print *, allocated(qkappa)
      call pinputDatabaseFileMAX(DSMconfFile,outputDir,psvmodel,modelname,tlen,rmin_,rmax_,rdelta_,r0min,r0max,r0delta,thetamin,thetamax,thetadelta,imin,imax,rsgtswitch,tsgtswitch,synnswitch,psgtswitch,re,ratc,ratl,omegai,maxlmax,deltalwindow,maxMemoryInGigabyte,tmpfile)
 
      print *, maxMemoryInGigabyte, tmpfile, DSMconfFile
