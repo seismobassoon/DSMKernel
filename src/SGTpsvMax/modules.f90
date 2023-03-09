071f48f92528bbf42f42a18c180bef20c9f0b6ca
Nobuaki Fuji <seismobassoon@gmail.com>
2023-03-09 17:47:04 +0100

    bizarre

--- src/SGTpsvMax/modules.f90
+++ src/SGTpsvMax/modules.f90
@@ -145,8 +145,10 @@ subroutine bcast_allocate_1
      call pinputDatabaseFileMAX(DSMconfFile,outputDir,psvmodel,modelname,tlen,rmin_,rmax_,rdelta_,r0min,r0max,r0delta,thetamin,thetamax,thetadelta,imin,imax,rsgtswitch,tsgtswitch,synnswitch,psgtswitch,re,ratc,ratl,omegai,maxlmax,deltalwindow,maxMemoryInGigabyte,tmpfile)
 
      print *, maxMemoryInGigabyte, tmpfile, DSMconfFile
-     if(allocated(qkappa))print *, "wow, qkappa is allocated  before timefilenamegenerator  in ", my_rank, qkappa
      print *, "no error is observed 09032023"
+     if(allocated(qkappa))print *, "wow, qkappa is allocated  before timefilenamegenerator  in ", my_rank, qkappa
+     print *, "no error is observed 09032023-2"
+
 
      
      !call readDSMconfFile(DSMconfFile,re,ratc,ratl,omegai,maxlmax)
