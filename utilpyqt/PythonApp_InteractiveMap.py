53a0b2e007791cceee758bf2ab5f4cf70752c828
lorraine_delaroque <105422516+LorrN13@users.noreply.github.com>
2023-03-09 15:47:56 +0100

    Add files via upload
    Modifications : il est possible de télécharger le fichier mseed de la trace brute ! Néanmoins, quelques avertissements : toutes les stations n'ont pas de donnée pour une période donnée. Cela entraîne donc un arrêt de l'application (une amélioration sera faite par rapport à cela). La période de temps est fixe encore (elle sera bientôt mise à jour, il sera possible de choisir soit même la durée de la trace). Le spectre et le traitement de signal ne sont pas encore disponibles.

--- utilpyqt/PythonApp_InteractiveMap.py
+++ utilpyqt/PythonApp_InteractiveMap.py
@@ -11,20 +11,22 @@ Created on Fri Feb  3 16:35:46 2023
 @author: Lorraine Delaroque
 """
 
-import sys, time
-
-import io
+import sys, time, os
 import pytz
-
+import numpy as np
 
 from obspy.clients.fdsn import Client
+from obspy import read, UTCDateTime
 
-from DataProcessor_Fonctions import get_depth_color # Load the function "plot_events" provided in tp_obsp
+from DataProcessor_Fonctions import get_depth_color, get_periodogram # Load the function "plot_events" provided in tp_obsp
 
-from PyQt5.QtWidgets import QMenu, QPushButton, QMessageBox, QDialog,QProgressBar, QFrame
+from PyQt5.QtWidgets import QMenu, QPushButton, QMessageBox, QDialog,QProgressBar, QFrame, QCheckBox
 from PyQt5.QtWidgets import QDateTimeEdit, QWidget,QVBoxLayout,QToolBar, QGridLayout,QListWidgetItem
 from PyQt5.QtWidgets import QAction, QLineEdit, QDoubleSpinBox, QTabWidget,QSlider,QListWidget
 from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QComboBox,QHBoxLayout,QDesktopWidget
+
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
 
 from pyqtlet import L, MapWidget 
@@ -262,6 +264,7 @@ class Window(QMainWindow):
         self.locChoice = QLineEdit()
         self.mainToolBar.addWidget(self.locChoice)
         self.locChoice.setMaxLength(5)
+        self.locChoice.setText("*")
         self.locChoice.setPlaceholderText("Enter the location")
                 
         # SEPARATING----------------------------------------------
@@ -511,6 +514,7 @@ class Window(QMainWindow):
             pyEndTime = localEndTime.toPyDateTime()
             UTCEndTime = pyEndTime.astimezone(pytz.UTC)
             
+            global start,end
             start = UTCStartTime
             end = UTCEndTime
 
@@ -526,6 +530,10 @@ class Window(QMainWindow):
             intMinLon = int(valueMinLon)
             valueMaxLon = self.maxLonChoice.value()
             intMaxLon = int(valueMaxLon)
+            
+            # LOCATION
+            global location
+            location = self.locChoice.text()
             
             # MAGNITUDE INITIALIZATION (convert QDoubleSpinBox to int)
             valueMagMin = self.magChoice.value()
@@ -554,7 +562,9 @@ class Window(QMainWindow):
 
             
             # NETWORK INITIALIZATION
+            global network_select
             network_select = networkValue
+            global client
             client = Client(client_select)
             inventory = client.get_stations(network=network_select, level="channel")
             
@@ -631,7 +641,7 @@ class Window(QMainWindow):
     
     def showDialog(self):
         self.dialog = QDialog()
-        label = QLabel('Ma fenêtre de liste')
+        label = QLabel('My stations list')
         label.setAlignment(Qt.AlignCenter)
         
         # Créer un QVBoxLayout
@@ -696,20 +706,24 @@ class ExamplePopup(QDialog):
         self.layout.addWidget(self.tabs)
         self.setLayout(self.layout)
         
+        self.name = name
+        
 
-        self._contentTab1(name)
+        self._contentTab1()
+        #self._contentTab2()
+        self._contentTab3()
         
         
-    def _contentTab1(self,name):
-        sectionName = QLabel("<b>Name:</b> {}".format(name))
+    def _contentTab1(self):
+        sectionName = QLabel("<b>Name:</b> {}".format(self.name))
         self.Description.layout.addWidget(sectionName)
         
         # FAIRE UN TRUC ICI POUR ARRANGER L'AFFICHAGE
 
-
+        # DESCRIPTION INFORMATION
         for i in stations:
             nameStation = i[0] + "." + i[1]
-            if nameStation==name:
+            if nameStation==self.name:
                 latStation = i[2]
                 lonStation = i[3]
                 elevStation = i[4]
@@ -720,21 +734,138 @@ class ExamplePopup(QDialog):
         elevation = QLabel("<b>Elevation:</b> {}".format(elevStation))
         self.Description.layout.addWidget(elevation)
         
+        # SEPARATOR
         separator = QFrame()
         separator.setFrameShape(QFrame.HLine)
         separator.setFrameShadow(QFrame.Sunken)
         self.Description.layout.addWidget(separator)
         
-        Channel = QLabel("<b> Channel </b>")
+        # CHANNEL INITIALIZATION
+        Channel = QLabel("<b>Channel: </b>")
         self.Description.layout.addWidget(Channel)
-        channelChoice = QLineEdit()
-        self.Description.layout.addWidget(channelChoice)
-        channelChoice.setPlaceholderText("Enter a channel")
+        self.channelChoice = QLineEdit()
+        self.Description.layout.addWidget(self.channelChoice)
+        self.channelChoice.setPlaceholderText("Enter a channel")
+        self.channel = self.channelChoice.text()
+        
+        # SEPARATOR
+        self.Description.layout.addWidget(separator)
+        
+        # DATE INITIALIZATION
+        date = QLabel("Period of the seismic trace")
+        self.Description.layout.addWidget(date)
+        dateMin = start
+        dateMax = end
+        
+        layoutTime = QHBoxLayout()
+        self.startTrace = QDateTimeEdit()
+        self.endTrace = QDateTimeEdit()
+        layoutTime.addWidget(self.startTrace)
+        layoutTime.addWidget(self.endTrace)
+        self.Description.layout.addLayout(layoutTime)
+        
+        # Connecter les signaux pour détecter les changements dans les champs
+        # Les seuls champs à remplir pour afficher la trace sont le channel, le starttime et le endtime
+        # Les autres sont "globaux" donc déjà avec une valeur.
+        self.channelChoice.editingFinished.connect(self.plot_seismic)
+        self.startTrace.editingFinished.connect(self.plot_seismic)
+        self.endTrace.editingFinished.connect(self.plot_seismic)
+        
+        # Création d'un widget pour afficher la graphique de la trace sismique
+        self.figure = Figure(figsize=(6,4))
+        self.canvas = FigureCanvas(self.figure)
+        self.Description.layout.addWidget(self.canvas)
+        
+        # DOWNLOAD DATA
+        self.download_button = QPushButton("Download",self)
+        self.Description.layout.addWidget(self.download_button)
+                
+        
+        
+        # FAIRE EN SORTE QUE CA AFFICHE 
+    def plot_seismic(self):
+            
+        if (self.channelChoice.text() and self.startTrace.dateTime() and self.endTrace.dateTime()):
+            '''
+            pyStartTime = self.startTrace.toPyDateTime()
+            UTCStartTime = pyStartTime.astimezone(pytz.UTC)
+            
+            pyEndTime = self.endTrace.toPyDateTime()
+            UTCEndTime = pyEndTime.astimezone(pytz.UTC)
+            '''
+            self.nameStation = self.name.split('.')[1]
+
+            
+            self.st = client.get_waveforms(
+                network = network_select,
+                station = self.nameStation,
+                location = location,
+                channel = self.channelChoice.text(),
+                starttime = UTCDateTime("2018-02-12T03:08:02"),
+                endtime = UTCDateTime("2018-02-12T03:08:02") +90,
+                attach_response = True,
+            )
+            
+            self.st.write('./trace.mseed')
+            st2 = read("trace.mseed")
+            self.figure.clear()
+            st2.plot(fig=self.figure)
+
+            self.canvas.draw()
+            os.remove('trace.mseed')
+            
+            self.download_button.clicked.connect(self.download_data)
     
+    def download_data(self):
+        # ESSAYER DE TROUVER UNE ALTERNATIVE POUR LE CHANNEL BH* OU BH?
+        self.st.write('./{}_{}.mseed'.format(network_select,self.nameStation))
+            
+            
+        # si la valeur change, plotter la trace
+        #self.plot_seismic(name,channel,startTrace,endTrace)
+        
+    def _contentTab2(self):
+        
+        x = self.st[0].data
+        sampling_rate = self.st[0].stats.sampling_rate
+        
+        # Print the sampling rate on the window
+        samplingLabel = QLabel("Sampling rate: {}".format(sampling_rate))
+        self.Periodogram.layout.addWidget(samplingLabel)
+        
+        
+        freqs,psd = get_periodogram(x, fs = sampling_rate, semilog = True, show = False)
+        
+        # Creation de la figure
+        
+        self.figure_spectrum = Figure(figsize=(6,4),dpi=100)
+        ax = self.figure_spectrum.add_subplot(111)
+        ax.semilogx(freqs, 10 * np.log10(psd))
+        ax.set_xlabel('Frequency (Hz)')
+        ax.set_ylabel('Power (dB)')
+        
+        self.canvas_spectrum = FigureCanvas(self.figure_spectrum)
+        self.Periodogram.layout.addWidget(self.canvas_spectrum)    
+        
+        self.canvas_spectrum.draw()
+        
+        #self.figure_spectrum.clear()
+        
+        
+
+    def _contentTab3(self):
+        instrumental_response = QCheckBox("Remove instrumental response")
+        self.Processing.layout.addWidget(instrumental_response)
+        
+        remove_mean = QCheckBox("Remove the mean")
+        self.Processing.layout.addWidget(remove_mean)
+        
+        
+        
 
         
-        
-        
+            
+  
 class SplashScreen(QMainWindow):
     def __init__(self):
         super().__init__()
